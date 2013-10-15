9f4b8e980401410ab46814703aa8665215470e8b
David Sergey <nirth.furzahad@gmail.com>
2013-10-15 15:19:33 +0100

    Linted code

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
-// Imports
-var less = require('less')
-    path = require('path');
+(function (less, path, module) {
+  'use strict';
+  // Module
 
-// Module
+  var createLessPreprocessor = function (args, config, logger, helper) {
+    config = config || {};
 
-var createLessPreprocessor = function (args, config, logger, helper) {
-  log.info("Processing LESS files");
-  config = confing || {};
+    var log = logger.create('preprocessor:less');
+    log.info("Processing LESS files");
 
-  var log = logger.create('preprocessor:less');
+    var tranformPath = args.transformPath || config.transformPath || function (filePath) {
+      return filePath.replace(/\.less$/, '.css');
+    };
 
-  var options = helper.merge({
-  }, args.options || {}, config.options || {});
+    var rendered = function (done, error, css) {
+      if (error !== null && error !== undefined) {
+        log.error('%s\n', error);
+      } else {
+        log.info("Processed");
+        done(css);
+      }
+    };
 
-  var tranformPath = args.transformPath || config.transformPath || function (filePath) {
-    return filePath.replace(/\.less$/, '.css');
+    return function (content, file, done) {
+      log.info("lessPreprocessor");
+      log.info('Processing "%s".', file.originalPath);
+      file.path = tranformPath(file.originalPath);
+
+      try {
+        less.render(content, rendered.bind(null, done));
+      } catch (error) {
+        log.error('%s\n  at %s', error.message, file.originalPath);
+        return;
+      }
+    };
   };
 
-  var rendered = function (done, error, css) {
-    if (error !== null && error !== undefined) {
-      log.error('%s\n', error);
-    } else {
-      log.info("Processed");
-      done(css);
-    }
-  }
+  createLessPreprocessor.$inject = ['args', 'config.lessPreprocessor', 'logger', 'helper'];
 
-  return function (content, file, done) {
-    log.info("lessPreprocessor");
-    log.info('Processing "%s".', file.originalPath);
-    file.path = transformPath(file.originalPath);
-
-    try {
-      less.render(content, rendered.bind(null, done));
-    } catch (error) {
-      log.error('%s\n  at %s', e.message, file.originalPath);
-      return;
-    }
-  }
-}
-
-createLessPreprocessor.$inject = ['args', 'config.lessPreprocessor', 'logger', 'helper'];
-
-// PUBLISH DI MODULE
-module.exports = {
-  'preprocessor:less': ['factory', createLessPreprocessor]
-};
+  // PUBLISH DI MODULE
+  module.exports = {
+    'preprocessor:less': ['factory', createLessPreprocessor]
+  };
+})(require('less'), require('path'), module);
