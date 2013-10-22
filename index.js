62bd68c515676a435e5cf86711fdf5d63f6bd6bd
nirth <nirth.furzahad@gmail.com>
2013-10-22 22:11:10 +0100

    Fixed parsing and compilation
     - Added paths option, for import searches.
     - Added compress option.

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-(function (less, path, module) {
-  'use strict';
-  // Module
+var less = require('less'),
+    Parser = less.Parser,
+    path = require('path');
 
-  var createLessPreprocessor = function (args, config, logger, helper) {
-    config = config || {};
-
-    var log = logger.create('preprocessor:less');
-    log.info("Processing LESS files");
-
-    var transformPath = args.transformPath || config.transformPath || function (filePath) {
-      return filePath.replace(/\.less$/, '.css');
-    };
-
-    var rendered = function (done, error, css) {
-      if (error !== null && error !== undefined) {
-        log.error('%s\n', error);
-      } else {
-        log.info("Processed");
-        done(css);
-      }
-    };
-
-    return function (content, file, done) {
-      log.info("lessPreprocessor");
-      log.info('Processing "%s".', file.originalPath);
-      file.path = transformPath(file.originalPath);
-
-      try {
-        less.render(content, rendered.bind(null, done));
-      } catch (error) {
-        log.error('%s\n  at %s', error.message, file.originalPath);
-        return;
-      }
-    };
+var createLessPreprocessor = function (args, config, logger, helper) {
+  config = config || {};
+  var options = config.options || {
+    compress: false
   };
 
-  createLessPreprocessor.$inject = ['args', 'config.lessPreprocessor', 'logger', 'helper'];
+  var log = logger.create('preprocessor:less');
 
-  // PUBLISH DI MODULE
-  module.exports = {
-    'preprocessor:less': ['factory', createLessPreprocessor]
+  var transformPath = args.transformPath || config.transformPath || function (filePath) {
+    return filePath.replace(/\.less$/, '.css');
   };
-})(
-  require('less'),
-  require('path'),
-  module
-);
+
+  var rendered = function (done, error, css) {
+    if (error !== null && error !== undefined) {
+      log.error('Error:%s\n', error);
+    } else {
+      done(css.toCSS({compress: options.compress}));
+    }
+  };
+
+  return function (content, file, done) {
+    file.path = transformPath(file.originalPath);
+
+    var parser = new Parser({
+      paths: options.paths
+    });
+
+    try {
+      parser.parse(content, rendered.bind(null, done));
+    } catch (error) {
+      log.error('%s\n  at %s', error.message, file.originalPath);
+      return;
+    }
+  };
+};
+
+createLessPreprocessor.$inject = ['args', 'config.lessPreprocessor', 'logger', 'helper'];
+
+// PUBLISH DI MODULE
+module.exports = {
+  'preprocessor:less': ['factory', createLessPreprocessor]
+};
